dd1541ef21da4fea1bfae6d022f6b50d613de3b6
陈鲁勇 <727057301@qq.com>
2018-09-04 17:36:55 +0800

    :sparkles:[ADD] add Bitcoin(Config)

--- src/main/java/com/bepal/coins/keytree/coins/Bitcoiner.java
+++ src/main/java/com/bepal/coins/keytree/coins/Bitcoiner.java
@@ -11,6 +11,7 @@ Bitcoiner
 */
 package com.bepal.coins.keytree.coins;
 
+import com.bepal.coins.keytree.config.CoinConfig;
 import com.bepal.coins.keytree.config.CoinConfigFactory;
 import com.bepal.coins.keytree.infrastructure.abstraction.ACoiner;
 import com.bepal.coins.keytree.infrastructure.tags.CoinTag;
@@ -25,5 +26,8 @@ public class Bitcoiner extends ACoiner {
             super(CoinConfigFactory.getConfig(
                     NetType.MAIN == netType ? CoinTag.tagBITCOIN : CoinTag.tagBITCOINTEST
             ));
+    }
+    public Bitcoiner(CoinConfig coinConfig) {
+        super(coinConfig);
     }
 }
